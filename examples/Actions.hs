ed328fa00ee6987065b6717382479d9d603e3124
lczm <zemingchua@gmail.com>
2023-05-01 17:36:41 +0800

    Lecture 8 Actions examples

---
+++ examples/Actions.hs
@@ -0,0 +1,16 @@
+module Actions where
+
+printTwoThings :: IO ()
+printTwoThings = do
+  putStrLn "Hello!"
+  putStrLn "How are you!"
+
+greet :: IO ()
+greet = do
+  putStrLn "What's your name?"
+  name <- getLine
+  putStrLn ("Hello, " ++ name)
+
+printAndIncrement :: Int -> Int
+printAndIncrement x = x + 1
+  where action = putStrLn "got a number!"
